62338c633a5345107c1bfb9025124c362845f8df
Azim1313 <84630581+Azim1313@users.noreply.github.com>
2021-10-04 12:14:07 +0300

    Add files via upload

---
+++ main-2.js
@@ -0,0 +1,32 @@
+document.querySelectorAll('.dropdown-link').forEach((elem) => {
+  elem.addEventListener('click', function(e) {
+      e.preventDefault();
+      const id = e.target.getAttribute('href').replace('#','');
+      document.querySelectorAll('.dropdown-link').forEach(
+          (element) => element.classList.remove('active')
+      );
+      document.querySelectorAll('.cart__container').forEach(
+          (el) => el.classList.add('hide')
+      );
+      elem.classList.add('active');
+      document.getElementById(id).classList.remove('hide');
+  });
+});
+
+
+!function(){"use strict";document.addEventListener("click",function(t){const e=t.target.closest("[data-easy-toggle]");if(e){t.preventDefault();let a=e.getAttribute("data-easy-toggle"),s=e.getAttribute("data-easy-class");try{document.querySelectorAll("[data-easy-toggle]").forEach(t=>{t.hasAttribute("data-easy-parallel")||t===e||document.querySelector(t.getAttribute("data-easy-toggle")).classList.remove(t.getAttribute("data-easy-class"))}),document.querySelector(a).classList.toggle(s)}catch(t){console.warn("EasyToggler.js : Блок "+a+" не существует")}}if(!e){let e=document.querySelectorAll("[data-easy-rcoe]");Array.from(e).forEach.call(e,function(e){let a=e.getAttribute("data-easy-toggle"),s=e.getAttribute("data-easy-class");if(!t.target.closest(a))try{document.querySelector(a).classList.remove(s)}catch(t){console.warn("EasyToggler.js : Блок "+a+" не существует")}})}})}();
+
+
+
+$(document).ready(function() {
+    $('.header__mobile__menu').click(function(event) {
+      $('.header__mobile__menu, .header__menu').toggleClass('active');
+    });
+  });
+
+// $(document).ready (function() {
+//   $('.slider__corusel').slick({
+//     arrows: false,
+//     dots: false
+//   });
+// });
---
+++ main-5.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+  $('.header__mobile__menu').click(function(event) {
+    $('.header__mobile__menu, .header__menu').toggleClass('active');
+  });
+});
+
